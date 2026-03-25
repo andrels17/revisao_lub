3048fea6fefc1f932ac8bc001a139fc3c399d403
andrels17 <andreluis369369@hotmail.com>
2026-03-25 10:31:40 -0300

    Delete revisao_lub-main directory

--- revisao_lub-main/database/connection.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-import streamlit as st
-
-def get_conn():
-    return psycopg2.connect(st.secrets["DATABASE_URL"])
--- revisao_lub-main/ui/dashboard_page.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def render():
-    st.title("Dashboard")
-    st.info("Base inicial carregada com sucesso.")
