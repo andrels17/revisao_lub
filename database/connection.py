2fa17aac8673d94bd03a52ee1d2877bf96276e53
andrels17 <andreluis369369@hotmail.com>
2026-03-25 10:09:11 -0300

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -2,4 +2,13 @@ import psycopg2
 import streamlit as st
 
 def get_conn():
-    return psycopg2.connect(st.secrets["DATABASE_URL"])
+    url = st.secrets["DATABASE_URL"]
+
+    # normaliza caso venha postgres://
+    if url.startswith("postgres://"):
+        url = url.replace("postgres://", "postgresql://", 1)
+
+    return psycopg2.connect(
+        url,
+        connect_timeout=10
+    )
