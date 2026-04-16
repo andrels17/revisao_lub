59da032552baa66912fd2a305d47664f8f271f83
andrels17 <andreluis369369@hotmail.com>
2026-04-16 14:30:55 -0300

    Add files via upload

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,27 @@
-from .formatters import (
-    format_dataframe_br,
-    format_decimal_br,
-    format_int_br,
-    format_medida_br,
-    format_metric_value,
-    format_numero_br,
-    format_percent_br,
-    install_streamlit_br_patch,
-)
+try:
+    from .formatters import (
+        format_dataframe_br,
+        format_decimal_br,
+        format_int_br,
+        format_medida_br,
+        format_metric_value,
+        format_numero_br,
+        format_percent_br,
+        install_streamlit_br_patch,
+    )
+except Exception:
+    from .formatters import (
+        format_dataframe_br,
+        format_decimal_br,
+        format_int_br,
+        format_medida_br,
+        format_metric_value,
+        format_numero_br,
+        format_percent_br,
+    )
+
+    def install_streamlit_br_patch(_st_module):
+        return None
 
 __all__ = [
     'format_dataframe_br',
